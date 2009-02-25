3213c69965737760cce273b6db1e6cb565f2ace6
jpetersson@sedoss.com <jpetersson@sedoss.com@d8a4333a-02e7-11de-9d1d-173cc1d06e3c>
2009-02-25 03:04:35 +0000

    Inital upload

---
+++ server.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+#
+#  OpenVPN Authserver -- An open framework to provide various authentication
+#                        methods to OpenVPN in a standardized way
+#
+#  Copyright (C) 2009 sedOSS <jpetersson@sedoss.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 2
+#  as published by the Free Software Foundation.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+
+use strict;
+use warnings;
